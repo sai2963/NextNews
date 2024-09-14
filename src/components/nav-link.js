9e225f459f4d5528fe9402458380053fc1ed5e6c
D V SAI <venkatsai2406@gmail.com>
2024-09-14 18:09:48 +0530

    image view done

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -10,7 +10,7 @@ export default function NavLink({ href, children }) {
         href={href}
         className={
           path.startsWith(href)
-            ? "text-l font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+            ? "text-lg font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-fuchsia-500 to-yellow-400"
             : undefined
         }
       >
