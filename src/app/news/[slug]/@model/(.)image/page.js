3800ecd21e849697bc954b8d96b7d83f4cc375b6
D V SAI <venkatsai2406@gmail.com>
2024-09-15 17:12:59 +0530

    intercepting and parallel routes added

---
+++ src/app/news/[slug]/@model/(.)image/page.js
@@ -0,0 +1,28 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default function ImageView({ params }) {
+  const newsId = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsId);
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">Intercepted Routes</h1>
+      <div className="m-10 p-2 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-xl">
+        <div className="border-4 border-transparent rounded-lg bg-white p-2">
+          <img
+            src={`/images/news/${newsItem.image}`}
+            alt={newsItem.title}
+            width={800}
+            height={450}
+            className="w-full h-auto rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
